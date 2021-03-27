d9952de33a64a4e49321579e660b24e83be863a3
andybyers21 <andy.byers21@gmail.com>
2021-03-27 13:18:15 +0000

    find smallest int

--- 3_arrays/main.go
+++ 3_arrays/main.go
@@ -8,11 +8,32 @@ import (
 )
 
 func main() {
-	percentageConvert()
+	// percentageConvert()
+	smallestInt()
 }
 
+// Write a program that finds the smallest number in an array:
+func smallestInt() {
+	x := []int{
+		48, 96, 86, 68,
+		57, 82, 63, 70,
+		37, 34, 83, 27,
+		19, 97, 9, 17,
+	}
+
+	h := x[0]
+
+	for i := 0; i <= len(x)-1; i++ {
+		if x[i] < h {
+			h = x[i]
+		}
+	}
+
+	fmt.Println(h)
+}
+
+// convert test scores out of 30 (in an array) to a percentage
 func percentageConvert() {
-	// convert test scores out of 30 (in an array) to a percentage
 
 	// test scores
 	score := []float64{
