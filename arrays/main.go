6415edf69ccc39396c577412c43b38d17f31b325
andybyers21 <andy.byers21@gmail.com>
2021-03-27 12:17:25 +0000

    add comments

--- arrays/main.go
+++ arrays/main.go
@@ -16,20 +16,27 @@ func percentageConvert() {
 
 	// test scores
 	score := []float64{
-		30, 14, 22, 28, 4,
+		30, 14, 22, 28, 4, 12,
 	}
 
-	var percentScore []float64
+	// create a slice to hold percentage scores
+	var percentScore = make([]float64, len(score))
+
+	// initialize a variable to calculate average score
 	var t float64
 
 	for i := 0; i < len(score); i++ {
+		// calculate scores as a percentage
 		percentScore[i] = (score[i] / 30) * 100
+		// round scores to nearest int
 		percentScore[i] = math.Round(percentScore[i])
+		// add all scores together
 		t += percentScore[i]
 	}
-
+	// calculate average of all scores
 	averageScore := t / float64(len(score))
 
+	// print results to the terminal
 	fmt.Println("all test scores: ", score)
 	fmt.Println("all scores by %: ", percentScore)
 	fmt.Println("average test score: ", averageScore, "%")
