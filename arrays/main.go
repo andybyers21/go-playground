05db17e166b6b2b1d2eb02cdeeb368787199880a
andybyers21 <andy.byers21@gmail.com>
2021-03-27 12:01:17 +0000

    make code more flexible

--- arrays/main.go
+++ arrays/main.go
@@ -15,23 +15,20 @@ func percentageConvert() {
 	// convert test scores out of 30 (in an array) to a percentage
 
 	// test scores
-	var score [5]float64
-	score[0] = 30
-	score[1] = 14
-	score[2] = 22
-	score[3] = 28
-	score[4] = 4
+	score := [5]float64{
+		30, 14, 22, 28, 4,
+	}
 
 	var percentScore [5]float64
 	var t float64
 
-	for i := 0; i < 5; i++ {
+	for i := 0; i < len(score); i++ {
 		percentScore[i] = (score[i] / 30) * 100
 		percentScore[i] = math.Round(percentScore[i])
 		t += percentScore[i]
 	}
 
-	averageScore := t / len(score)
+	averageScore := t / float64(len(score))
 
 	fmt.Println("all test scores: ", score)
 	fmt.Println("all scores by %: ", percentScore)
